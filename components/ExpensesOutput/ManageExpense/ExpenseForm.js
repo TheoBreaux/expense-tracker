978f8a85b080c4b80e591d961aabd43831510501
Theo Breaux <theobreaux@gmail.com>
2024-08-02 14:48:11 -0700

    inputs

---
+++ components/ExpensesOutput/ManageExpense/ExpenseForm.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import React from "react";
+import Input from "./Input";
+
+const ExpenseForm = () => {
+  return (
+    <View>
+      <Input />
+    </View>
+  );
+};
+
+export default ExpenseForm;
+
+const styles = StyleSheet.create({});
